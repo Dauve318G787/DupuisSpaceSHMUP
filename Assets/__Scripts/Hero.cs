ff239bafa72e2efc5f090263307a26b5d1582091
Dauve318G787 <davbekdup@gmail.com>
2025-03-27 20:27:12 -0500

    Done up to 796

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -14,7 +14,10 @@ public class Hero : MonoBehaviour
     public float pitchMult = 30;
 
     [Header("Dynamic")] [Range(0,4)]
-    public float shieldLevel = 1;
+    private float _shieldLevel = 1;
+    //public float shieldLevel = 1;
+    [Tooltip("This field holds a reference to the last triggering GameObject" )]
+    private GameObject  lastTriggerGo = null;
 
     void Awake() {
         if (S == null) {
@@ -36,4 +39,32 @@ public class Hero : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
     }
-}
+
+    void OnTriggerEnter(Collider other) {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        Debug.Log("Shield trigger hit by: " + go.gameObject.name );
+
+        if( go == lastTriggerGo ) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null) {
+            shieldLevel--;
+            Destroy(go);
+        } else {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: " + go.name);
+        }
+    }
+
+    public float shieldLevel {
+        get { return ( _shieldLevel ); }
+        private set {
+            _shieldLevel = Mathf.Min( value, 4 );
+
+            if (value < 0) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    }
